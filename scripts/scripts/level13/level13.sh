72441aee572e185bc5f3982fc7c8ce4647232f44
TheOzzy <kosmann64@googlemail.com>
2026-02-27 10:21:34 +0000

    Level 13 Complete

--- scripts/scripts/level13/level13.sh
+++ scripts/scripts/level13/level13.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
 DIR="./backups"
-SOURCE="/home/vboxuser/MyDevOpsToolKit-WH/scripts/scripts/logfiles"
+SOURCE="/home/osman/Documents/Github/MyDevOpsToolKit-WH/scripts/scripts/logfiles"
 TIME_STAMP=$(date +%Y%m%d_%H%M%S)
 NEW_BACKUP="$DIR/backup_$TIME_STAMP"
 
 cp -r "$SOURCE" "$NEW_BACKUP"
 
-echo "Copy completed
+echo "Copy completed"
+
+ls -dt "$DIR"/backup_* | tail -n +6 | xargs rm -rf
+
+
+
+
